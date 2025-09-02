55824d1ff14cfc638fc0ab299634ffd8ee54459f
maijo-hub <203755423+maijo-hub@users.noreply.github.com>
2025-09-02 05:18:41 +0000

    じゃんけん

---
+++ chapter3/app.js
@@ -0,0 +1,13 @@
+function getJShand(){
+  let js_hand_num = Math.floor( Math.ramdom() *3 );
+
+  if (js_hand_num == 0){
+    js_hand = "グー";
+  } else if(js_hand_num = 1){
+    js_hand = "チョキ";
+  } else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+
+  return js_hand;
+}
